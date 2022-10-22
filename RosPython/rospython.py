6e1a83edf330763194375c254a7608442bef7220
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2022-10-21 23:53:02 -0500

    Created main script on RosPython

---
+++ RosPython/rospython.py
@@ -0,0 +1,8 @@
+# main script for controlling the robot using ROS and Python
+#contains the main loop and the callback functions for the subscribers
+
+# import the necessary modules
+import rospy
+
+# conections to ROS
+
